47cf7dc82db927ec8f1c606bedb0d769d793e10e
Rehan <rehan.vdm4@gmial.com>
2021-05-30 23:24:26 +0200

    Cleanup before deploy Add favicon Add OG meta tags

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -1,12 +1,37 @@
-const { description } = require('../../package')
+let title = 'WarpURL API Documentation';
+let description = title;
+// const image = path.resolve(__dirname + '/static/logo_back_white.svg');
+let image = 'https://warpurl.com/cdn/logos/logo_back_white.png';
+
+let ogTagsFromWarpUrlWeb = [
+  { hid: 'twitter:title', name: 'twitter:title', content: title },
+  { hid: 'twitter:description', name: 'twitter:description', content: description },
+  { hid: 'twitter:image', name: 'twitter:image', content: image },
+  { hid: 'twitter:image:alt', name: 'twitter:image:alt', content: title },
+  { hid: 'og:title', property: 'og:title', content: title },
+  { hid: 'og:site_name', property: 'og:site_name', content: title },
+  { hid: 'og:description', property: 'og:description', content: description },
+  { hid: 'og:image', property: 'og:image', content: image },
+  { hid: 'og:image:secure_url', property: 'og:image:secure_url', content: image },
+  { hid: 'og:image:alt', property: 'og:image:alt', content: title }
+];
+
+let ogTagsTransformed = ogTagsFromWarpUrlWeb.map(row => {
+  return ['meta', row ];
+})
 
 module.exports = {
-  title: '',
+  title: title,
   description: description,
   head: [
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui' }],
+    ['meta', { name: 'robots', content: 'noindex,nofollow' }],
+    ['meta', { name: 'author', content: 'Rehan van der Merwe' }],
+    ...ogTagsTransformed,
+    ['meta', { name: 'theme-color', content: '#2a7bff' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'icon', href: '/favicon-32x32.png' }]
   ],
   themeConfig: {
     logo: '/logo_back_white.svg',
